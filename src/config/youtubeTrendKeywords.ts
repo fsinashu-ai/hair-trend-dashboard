d862b8562f545d615e164df9ba2feccc7483cd71
fsinashu-ai <f.sinashu@gmail.com>
2026-05-14 21:16:59 +0900

    Add YouTube trend keyword config

---
+++ src/config/youtubeTrendKeywords.ts
@@ -0,0 +1,18 @@
+export const defaultYoutubeTrendKeywords = [
+  "髪質改善",
+  "縮毛矯正",
+  "白髪ぼかし",
+  "大人ショート",
+  "韓国ヘア",
+  "ボブ",
+  "レイヤー",
+  "美容師 集客",
+  "美容室 Instagram",
+];
+
+export const youtubeTrendSearchConfig = {
+  dailyVideoLimit: 6,
+  keywordLimit: 4,
+  maxResultsPerKeyword: 3,
+  rangeDays: 7,
+};
