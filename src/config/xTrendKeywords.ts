5be0d4e8e7ae91316034d7f5043f6cacd2171efe
fsinashu-ai <f.sinashu@gmail.com>
2026-06-18 11:01:28 +0900

    Add X trend keyword config

---
+++ src/config/xTrendKeywords.ts
@@ -0,0 +1,17 @@
+export const defaultXTrendKeywords = [
+  "髪質改善",
+  "縮毛矯正",
+  "白髪ぼかし",
+  "大人ショート",
+  "韓国ヘア",
+  "ボブ",
+  "レイヤー",
+  "美容師 集客",
+  "美容室 Instagram",
+];
+
+export const xTrendSearchConfig = {
+  keywordLimit: 5,
+  maxResultsPerKeyword: 5,
+  runPostLimit: 20,
+};
